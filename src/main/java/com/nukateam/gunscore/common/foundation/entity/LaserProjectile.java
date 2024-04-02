75cf66a089f3ee5af687ba5d43da7354d7f24144
Jetug <ksergeev01@gmail.com>
2024-04-02 15:39:13 +0300

    fixes

--- src/main/java/com/nukateam/gunscore/common/foundation/entity/LaserProjectile.java
+++ src/main/java/com/nukateam/gunscore/common/foundation/entity/LaserProjectile.java
@@ -11,10 +11,18 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.BaseFireBlock;
+import net.minecraft.world.level.block.CampfireBlock;
+import net.minecraft.world.level.block.CandleBlock;
+import net.minecraft.world.level.block.CandleCakeBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.phys.Vec3;
 
 public class LaserProjectile extends AbstractBeamProjectile {
+    private static final float GROUND_FIRE_CHANCE = 0.1f;
+    private static final float ENTITY_FIRE_CHANCE = 0.1f;
+
     public LaserProjectile(EntityType<? extends ProjectileEntity> entityType, Level worldIn) {
         super(entityType, worldIn);
     }
@@ -41,18 +49,37 @@ public class LaserProjectile extends AbstractBeamProjectile {
 //        }
 //    }
 
-//    @Override
-//    protected void onHitEntity(Entity entity, Vec3 hitVec, Vec3 startVec, Vec3 endVec, boolean headshot) {
-//        createExplosion(this, Config.COMMON.missiles.explosionRadius.get().floatValue(), false);
-//    }
-//
-//    @Override
-//    protected void onHitBlock(BlockState state, BlockPos pos, Direction face, double x, double y, double z) {
-//        createExplosion(this, Config.COMMON.missiles.explosionRadius.get().floatValue(), false);
-//    }
-//
-//    @Override
-//    public void onExpired() {
-//        createExplosion(this, Config.COMMON.missiles.explosionRadius.get().floatValue(), false);
-//    }
+    public float getBlockFireChance(){
+        return GROUND_FIRE_CHANCE;
+    }
+
+    public float getEntityFireChance(){
+        return ENTITY_FIRE_CHANCE;
+    }
+
+    @Override
+    protected void onHitEntity(Entity entity, Vec3 hitVec, Vec3 startVec, Vec3 endVec, boolean headshot) {
+        super.onHitEntity(entity, hitVec, startVec, endVec, headshot);
+        if(random.nextFloat() <= getEntityFireChance())
+            entity.setRemainingFireTicks(20);
+    }
+
+    @Override
+    protected void onHitBlock(BlockState blockstate, BlockPos blockpos, Direction face, double x, double y, double z) {
+        super.onHitBlock(blockstate, blockpos, face, x, y, z);
+
+        if(random.nextFloat() <= getBlockFireChance()) {
+            if (!CampfireBlock.canLight(blockstate) && !CandleBlock.canLight(blockstate) && !CandleCakeBlock.canLight(blockstate)) {
+                var blockpos1 = blockpos.relative(face);
+
+                if (BaseFireBlock.canBePlacedAt(level, blockpos1, face)) {
+                    var blockstate1 = BaseFireBlock.getState(level, blockpos1);
+                    level.setBlock(blockpos1, blockstate1, 11);
+
+                }
+            } else {
+                level.setBlock(blockpos, blockstate.setValue(BlockStateProperties.LIT, Boolean.valueOf(true)), 11);
+            }
+        }
+    }
 }
