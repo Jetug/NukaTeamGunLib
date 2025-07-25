1c17c8e52c090a627ea873cfe0a9d5b3e26e407f
Jetug <ksergeev01@gmail.com>
2025-07-25 19:50:29 +0300

    save

--- src/main/java/com/nukateam/ntgl/client/util/handler/ClientGrenadeHandler.java
+++ src/main/java/com/nukateam/ntgl/client/util/handler/ClientGrenadeHandler.java
@@ -25,11 +25,14 @@ public class ClientGrenadeHandler {
             if(minecraft.options.keyAttack.isDown()){
                 var mainHand = minecraft.player.getMainHandItem();
                 var offHand = minecraft.player.getOffhandItem();
-                if(mainHand.getItem() instanceof IThrowable){
+                if(mainHand.getItem() instanceof IThrowable && !TRACKER_MAP.containsKey(InteractionHand.MAIN_HAND)){
 
                 }
                 else
             }
+            TRACKER_MAP.forEach((k, v) ->{
+                v.tick();
+            });
         }
     }
 
@@ -41,11 +44,11 @@ public class ClientGrenadeHandler {
         private final int maxThrow;
         private final int maxLife;
 
+        private int prepareTick = 0;
         private int throwTick = 0;
-        private int timeLeft = 0;
         private int lifeTick = 0;
 
-        private boolean isPreparing = false;
+        private boolean isPreparing = true;
         private boolean isThrowing = false;
 
         private Tracker(LivingEntity entity, InteractionHand arm) {
@@ -55,11 +58,46 @@ public class ClientGrenadeHandler {
             this.maxPrepare = throwable.getConfig().getGeneral().getPrepareTime();
             this.maxThrow = throwable.getConfig().getGeneral().getThrowTime();
             this.maxLife = throwable.getConfig().getProjectile().getLife();
+
+            prepareTick = maxPrepare;
+            throwTick = maxThrow;
+            lifeTick = maxLife;
         }
 
-//        public void tick(){
-//            if()
-//        }
+        public void tick(){
+            prepareTick = Math.max(prepareTick - 1, 0);
+
+            if(prepareTick == 0){
+                isPreparing = false;
+                lifeTick = Math.max(lifeTick - 1, 0);
+
+                if(lifeTick == 0){
+                    explode();
+                }
+
+                if (isThrowing){
+                    throwTick = Math.max(throwTick - 1, 0);
+                }
+            }
+
+
+//            throwTick = Math.max(prepareTick - 1, 0);
+        }
+
+        public void onRelease(){
+            if(prepareTick == 0){
+                isThrowing = true;
+                throwItem();
+            }
+        }
+
+        private void throwItem(){
+
+        }
+
+        private void explode() {
+
+        }
 
         private boolean isSameWeapon(LivingEntity entity) {
             return !this.stack.isEmpty() && entity.getItemInHand(arm) == this.stack;
