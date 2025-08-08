5530a378ca1ce45701e923dae231fc6f89f9547c
Chizeel <ksergeev01@gmail.com>
2025-08-08 16:40:31 +0300

    Improved grenade spawn

--- src/main/java/com/nukateam/ntgl/client/render/renderers/projectiles/ThrowableGrenadeRenderer.java
+++ src/main/java/com/nukateam/ntgl/client/render/renderers/projectiles/ThrowableGrenadeRenderer.java
@@ -20,6 +20,9 @@ import javax.annotation.Nullable;
  * Author: MrCrayfish
  */
 public class ThrowableGrenadeRenderer extends EntityRenderer<ThrowableGrenadeEntity> {
+
+    public static final int MAX_SIZE_TICK = 5;
+
     public ThrowableGrenadeRenderer(EntityRendererProvider.Context context) {
         super(context);
     }
@@ -33,28 +36,36 @@ public class ThrowableGrenadeRenderer extends EntityRenderer<ThrowableGrenadeEnt
     @Override
     public void render(ThrowableGrenadeEntity entity, float entityYaw, float partialTicks, PoseStack poseStack, MultiBufferSource renderTypeBuffer, int light) {
         poseStack.pushPose();
+        {
+            /* Makes the grenade face in the direction of travel */
+            poseStack.mulPose(Axis.YP.rotationDegrees(180F));
+            poseStack.mulPose(Axis.YP.rotationDegrees(entityYaw));
 
-        /* Makes the grenade face in the direction of travel */
-        poseStack.mulPose(Axis.YP.rotationDegrees(180F));
-        poseStack.mulPose(Axis.YP.rotationDegrees(entityYaw));
+            /* Offsets to the center of the grenade before applying rotation */
+            var rotation = entity.prevRotation + (entity.rotation - entity.prevRotation) * partialTicks;
+            poseStack.translate(0, 0.15, 0);
+            poseStack.mulPose(Axis.XP.rotationDegrees(-rotation));
+            poseStack.translate(0, -0.15, 0);
 
-        /* Offsets to the center of the grenade before applying rotation */
-        var rotation = entity.prevRotation + (entity.rotation - entity.prevRotation) * partialTicks;
-        poseStack.translate(0, 0.15, 0);
-        poseStack.mulPose(Axis.XP.rotationDegrees(-rotation));
-        poseStack.translate(0, -0.15, 0);
+            var scale = 1f;
 
-        if (entity instanceof StunGrenadeEntity) {
-            poseStack.translate(0, entity.getDimensions(Pose.STANDING).height / 2, 0);
-            poseStack.mulPose(Axis.ZP.rotationDegrees(-90F));
-            poseStack.translate(0, -entity.getDimensions(Pose.STANDING).height / 2, 0);
+            if(entity.tickCount < MAX_SIZE_TICK) {
+                scale = (entity.tickCount + partialTicks) / MAX_SIZE_TICK;
+            }
+
+            poseStack.scale(scale, scale, scale);
+
+            if (entity instanceof StunGrenadeEntity) {
+                poseStack.translate(0, entity.getDimensions(Pose.STANDING).height / 2, 0);
+                poseStack.mulPose(Axis.ZP.rotationDegrees(-90F));
+                poseStack.translate(0, -entity.getDimensions(Pose.STANDING).height / 2, 0);
+            }
+
+            /* */
+            poseStack.translate(0.0, 0.5, 0.0);
+
+            Minecraft.getInstance().getItemRenderer().renderStatic(entity.getItem(), ItemDisplayContext.NONE, light, OverlayTexture.NO_OVERLAY, poseStack, renderTypeBuffer, entity.level(), 0);
         }
-
-        /* */
-        poseStack.translate(0.0, 0.5, 0.0);
-
-        Minecraft.getInstance().getItemRenderer().renderStatic(entity.getItem(), ItemDisplayContext.NONE, light, OverlayTexture.NO_OVERLAY, poseStack, renderTypeBuffer, entity.level(), 0);
-
         poseStack.popPose();
     }
 }
