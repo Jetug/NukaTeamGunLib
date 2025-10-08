f24642a5dc1b778f2ae0f04236823738adbe6a05
Diego32099 <diegomaciel3000@gmail.com>
2025-10-08 20:54:14 -0300

    Update ModGuns.java to 1.21.1 deferredHolder (I used that to be more general instead of deferredItem)

--- src/main/java/com/nukateam/example/common/registery/ModGuns.java
+++ src/main/java/com/nukateam/example/common/registery/ModGuns.java
@@ -6,108 +6,111 @@ import com.nukateam.ntgl.common.data.attachment.impl.*;
 import com.nukateam.ntgl.common.foundation.item.*;
 import com.nukateam.ntgl.Ntgl;
 import com.nukateam.ntgl.common.util.interfaces.IGunModifier;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.world.item.Item;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.registries.*;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
 
 public class ModGuns {
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Ntgl.MOD_ID);
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(Registries.ITEM, Ntgl.MOD_ID);
     ///GUNS
-    public static final RegistryObject<WeaponItem> PISTOL10MM = registerGun("pistol10mm", new TestModifier());
-    public static final RegistryObject<WeaponItem> PIPE_PISTOL = registerGun("pipepistol");
-    public static final RegistryObject<WeaponItem> CLASSIC10MM = registerGun("classic10mm", 10);
-    public static final RegistryObject<WeaponItem> SCOUT10MM = registerGun("scout10mm");
-    public static final RegistryObject<WeaponItem> PIPE_REVOLVER = registerGun("piperevolver");
-    public static final RegistryObject<WeaponItem> FATMAN = registerGun("fatman");
-    public static final RegistryObject<WeaponItem> MINIGUN = registerGun("minigun", new MinigunModifier());
-    public static final RegistryObject<WeaponItem> POWDERGUN = registerGun("powdergun");
-    public static final RegistryObject<WeaponItem> SHOTGUN = registerGun("shotgun");
-    public static final RegistryObject<WeaponItem> FLAMER = registerGun("flamer");
-    public static final RegistryObject<WeaponItem> GATLING = registerGun("gatling");
-    public static final RegistryObject<WeaponItem> REVOLVER = registerGun("revolver");
-    public static final RegistryObject<WeaponItem> RPG = registerGun("rpg");
-    public static final RegistryObject<WeaponItem> HAMMER = registerGun("hammer");
+    public static final DeferredHolder<Item, WeaponItem> PISTOL10MM = registerGun("pistol10mm", new TestModifier());
+    public static final DeferredHolder<Item, WeaponItem> PIPE_PISTOL = registerGun("pipepistol");
+    public static final DeferredHolder<Item, WeaponItem> CLASSIC10MM = registerGun("classic10mm", 10);
+    public static final DeferredHolder<Item, WeaponItem> SCOUT10MM = registerGun("scout10mm");
+    public static final DeferredHolder<Item, WeaponItem> PIPE_REVOLVER = registerGun("piperevolver");
+    public static final DeferredHolder<Item, WeaponItem> FATMAN = registerGun("fatman");
+    public static final DeferredHolder<Item, WeaponItem> MINIGUN = registerGun("minigun", new MinigunModifier());
+    public static final DeferredHolder<Item, WeaponItem> POWDERGUN = registerGun("powdergun");
+    public static final DeferredHolder<Item, WeaponItem> SHOTGUN = registerGun("shotgun");
+    public static final DeferredHolder<Item, WeaponItem> FLAMER = registerGun("flamer");
+    public static final DeferredHolder<Item, WeaponItem> GATLING = registerGun("gatling");
+    public static final DeferredHolder<Item, WeaponItem> REVOLVER = registerGun("revolver");
+    public static final DeferredHolder<Item, WeaponItem> RPG = registerGun("rpg");
+    public static final DeferredHolder<Item, WeaponItem> HAMMER = registerGun("hammer");
 
-    public static final RegistryObject<Item> GRENADE = ITEMS.register("grenade",
+    public static final DeferredHolder<Item, Item> GRENADE = ITEMS.register("grenade",
             () -> new ThrowableItem(new Item.Properties()));
 
-//    public static final RegistryObject<Item> MISSILE = ITEMS.register("missile",
+//    public static final DeferredHolder<Item, Item> MISSILE = ITEMS.register("missile",
 //            () -> new AmmoItem(new Item.Properties().tab(ModItemTabs.WEAPONS)));
 
-    public static final RegistryObject<Item> GRENADE_OG = ITEMS.register("grenade_old",
+    public static final DeferredHolder<Item, Item> GRENADE_OG = ITEMS.register("grenade_old",
             () -> new ThrowableItem(new Item.Properties()));
 
-    public static final RegistryObject<Item> STUN_GRENADE = ITEMS.register("stun_grenade",
+    public static final DeferredHolder<Item, Item> STUN_GRENADE = ITEMS.register("stun_grenade",
             () -> new StunGrenadeItem(new Item.Properties()));
 
     //Rounds
-    public static final RegistryObject<Item> ROUND10MM = ITEMS.register("round10mm",
+    public static final DeferredHolder<Item, Item> ROUND10MM = ITEMS.register("round10mm",
             () -> new AmmoItem(new Item.Properties().durability(100)));
 
-    public static final RegistryObject<Item> ROUND38    = registerAmmo("round38"    );
-    public static final RegistryObject<Item> STEELBALLS = registerAmmo("steel_ball" );
-    public static final RegistryObject<Item> ROUND45    = registerAmmo("round45"    );
-    public static final RegistryObject<Item> ROUND5MM   = registerAmmo("round5mm"   );
-    public static final RegistryObject<Item> ROUND44    = registerAmmo("round44"    );
-    public static final RegistryObject<Item> ROUND50    = registerAmmo("round50"    );
-    public static final RegistryObject<Item> ROUND380   = registerAmmo("round380"   );
-    public static final RegistryObject<Item> ROUND556   = registerAmmo("round556"   );
-    public static final RegistryObject<Item> SHOTSHELL  = registerAmmo("shotshell"  );
-    public static final RegistryObject<Item> ROUND127   = registerAmmo("round127"   );
-    public static final RegistryObject<Item> ROUND22    = registerAmmo("round22"    );
-    public static final RegistryObject<Item> MININUKE   = registerAmmo("mini_nuke"  );
-    public static final RegistryObject<Item> FUEL       = registerAmmo("fuel"  );
+    public static final DeferredHolder<Item, Item> ROUND38    = registerAmmo("round38"    );
+    public static final DeferredHolder<Item, Item> STEELBALLS = registerAmmo("steel_ball" );
+    public static final DeferredHolder<Item, Item> ROUND45    = registerAmmo("round45"    );
+    public static final DeferredHolder<Item, Item> ROUND5MM   = registerAmmo("round5mm"   );
+    public static final DeferredHolder<Item, Item> ROUND44    = registerAmmo("round44"    );
+    public static final DeferredHolder<Item, Item> ROUND50    = registerAmmo("round50"    );
+    public static final DeferredHolder<Item, Item> ROUND380   = registerAmmo("round380"   );
+    public static final DeferredHolder<Item, Item> ROUND556   = registerAmmo("round556"   );
+    public static final DeferredHolder<Item, Item> SHOTSHELL  = registerAmmo("shotshell"  );
+    public static final DeferredHolder<Item, Item> ROUND127   = registerAmmo("round127"   );
+    public static final DeferredHolder<Item, Item> ROUND22    = registerAmmo("round22"    );
+    public static final DeferredHolder<Item, Item> MININUKE   = registerAmmo("mini_nuke"  );
+    public static final DeferredHolder<Item, Item> FUEL       = registerAmmo("fuel"  );
 
     /* Scope Attachments */
-    public static final RegistryObject<Item> HOLOGRAPHIC_SIGHT = ITEMS.register("holographic_sight",
+    public static final DeferredHolder<Item, Item> HOLOGRAPHIC_SIGHT = ITEMS.register("holographic_sight",
             () -> new ScopeItem(Attachments.LONG_SCOPE, new Item.Properties().stacksTo(1)));
 
-    public static final RegistryObject<Item> COLLIMATOR_SIGHT = ITEMS.register("collimator_sight",
+    public static final DeferredHolder<Item, Item> COLLIMATOR_SIGHT = ITEMS.register("collimator_sight",
             () -> new ScopeItem(Attachments.SHORT_SCOPE, new Item.Properties().stacksTo(1)));
 
     /* Barrel Attachments */
-    public static final RegistryObject<Item> SILENCER = ITEMS.register("silencer",
+    public static final DeferredHolder<Item, Item> SILENCER = ITEMS.register("silencer",
             () -> new BarrelItem(Barrel.create(8.0F, GunModifiers.SILENCED, GunModifiers.REDUCED_DAMAGE), new Item.Properties().stacksTo(1)));
 
     /* Stock Attachments */
-    public static final RegistryObject<Item> LIGHT_STOCK = ITEMS.register("light_stock",
+    public static final DeferredHolder<Item, Item> LIGHT_STOCK = ITEMS.register("light_stock",
             () -> new StockItem(Stock.create(GunModifiers.BETTER_CONTROL), new Item.Properties().stacksTo(1)));
-    public static final RegistryObject<Item> TACTICAL_STOCK = ITEMS.register("tactical_stock",
+    public static final DeferredHolder<Item, Item> TACTICAL_STOCK = ITEMS.register("tactical_stock",
             () -> new StockItem(Stock.create(GunModifiers.STABILISED), new Item.Properties().stacksTo(1)));
-    public static final RegistryObject<Item> WEIGHTED_STOCK = ITEMS.register("weighted_stock",
+    public static final DeferredHolder<Item, Item> WEIGHTED_STOCK = ITEMS.register("weighted_stock",
             () -> new StockItem(Stock.create(GunModifiers.SUPER_STABILISED), new Item.Properties().stacksTo(1)));
 
     /* Under Barrel Attachments */
-    public static final RegistryObject<Item> LIGHT_GRIP = ITEMS.register("light_grip",
+    public static final DeferredHolder<Item, Item> LIGHT_GRIP = ITEMS.register("light_grip",
             () -> new GripItem(Grip.create(GunModifiers.LIGHT_RECOIL), new Item.Properties().stacksTo(1)));
-    public static final RegistryObject<Item> SPECIALISED_GRIP = ITEMS.register("specialised_grip",
+    public static final DeferredHolder<Item, Item> SPECIALISED_GRIP = ITEMS.register("specialised_grip",
             () -> new GripItem(Grip.create(GunModifiers.REDUCED_RECOIL), new Item.Properties().stacksTo(1)));
 
     /* Magazine Attachments*/
-    public static final RegistryObject<Item> EXTENDED_MAGAZINE = ITEMS.register("extended_magazine",
+    public static final DeferredHolder<Item, Item> EXTENDED_MAGAZINE = ITEMS.register("extended_magazine",
             () -> new MagazineItem(Magazine.create(30, GunModifiers.SLOW_ADS), new Item.Properties().stacksTo(1)));
 
-    public static final RegistryObject<Item> DRUM_MAGAZINE = ITEMS.register("drum_magazine",
+    public static final DeferredHolder<Item, Item> DRUM_MAGAZINE = ITEMS.register("drum_magazine",
             () -> new MagazineItem(Magazine.create(60, GunModifiers.SLOWER_ADS, GunModifiers.EXTENDED_MAG), new Item.Properties().stacksTo(1)));
 
-    public static final RegistryObject<Item> HEAD_STONE = ITEMS.register("hammer_stone",
+    public static final DeferredHolder<Item, Item> HEAD_STONE = ITEMS.register("hammer_stone",
             () -> new GripItem(Grip.create(GunModifiers.REDUCED_RECOIL), new Item.Properties().stacksTo(1)));
 
-    public static final RegistryObject<Item> HAMMER_DIAMOND = ITEMS.register("hammer_diamond",
+    public static final DeferredHolder<Item, Item> HAMMER_DIAMOND = ITEMS.register("hammer_diamond",
             () -> new GripItem(Grip.create(GunModifiers.REDUCED_RECOIL), new Item.Properties().stacksTo(1)));
 
-    public static final RegistryObject<Item> AMMO_BOX = ITEMS.register("ammo_box", () ->
+    public static final DeferredHolder<Item, Item> AMMO_BOX = ITEMS.register("ammo_box", () ->
             new AmmoBoxItem(new Item.Properties().stacksTo(1), 100));
 
-    public static RegistryObject<WeaponItem> registerGun(String name, IGunModifier... modifiers) {
+    public static DeferredHolder<Item, WeaponItem> registerGun(String name, IGunModifier... modifiers) {
         return ITEMS.register(name, () -> new WeaponItem(new Item.Properties().stacksTo(1), modifiers));
     }
 
-    public static RegistryObject<WeaponItem> registerGun(String name, int durability) {
+    public static DeferredHolder<Item, WeaponItem> registerGun(String name, int durability) {
         return ITEMS.register(name, () -> new WeaponItem(new Item.Properties().durability(durability)));
     }
 
-    public static RegistryObject<Item> registerAmmo(String name) {
+    public static DeferredHolder<Item, Item> registerAmmo(String name) {
         return ITEMS.register(name, () -> new AmmoItem(new Item.Properties()));
     }
 
