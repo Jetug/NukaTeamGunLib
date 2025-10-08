72150f0b2c5b3bf490352e3b6d5a2c53cd18d44d
Diego32099 <diegomaciel3000@gmail.com>
2025-10-08 20:55:17 -0300

    Update CommonProxy.java to neoforge

--- src/main/java/com/nukateam/ntgl/CommonProxy.java
+++ src/main/java/com/nukateam/ntgl/CommonProxy.java
@@ -1,27 +1,25 @@
 package com.nukateam.ntgl;
 
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.event.TickEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.ClientTickEvent;
 
 import java.util.HashMap;
 
 import static com.nukateam.ntgl.ClientProxy.damageTypes;
 
-@Mod.EventBusSubscriber(modid = Ntgl.MOD_ID, value = Dist.CLIENT)
+@EventBusSubscriber(modid = Ntgl.MOD_ID, value = Dist.CLIENT)
 public class CommonProxy {
     @SubscribeEvent
-    public static void onServerTick(TickEvent.ClientTickEvent event) {
-        if (event.phase == TickEvent.Phase.START) {
-            var buffMap = new HashMap<>(damageTypes);
+    public static void onServerTick(ClientTickEvent.Pre event) {
+        var buffMap = new HashMap<>(damageTypes);
 
-            buffMap.forEach((key, value) -> {
+        buffMap.forEach((key, value) -> {
                 if (value.ticks <= 0) {
                     damageTypes.remove(key);
                 }
                 value.ticks--;
-            });
-        }
+        });
     }
 }
