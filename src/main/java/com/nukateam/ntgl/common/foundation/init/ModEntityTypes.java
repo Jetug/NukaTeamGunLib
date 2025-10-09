7002bb20164f9f1701ce8562babf161f85af5548
Diego32099 <diegomaciel3000@gmail.com>
2025-10-08 21:41:59 -0300

    Update ModEntityTypes.java

--- src/main/java/com/nukateam/ntgl/common/foundation/init/ModEntityTypes.java
+++ src/main/java/com/nukateam/ntgl/common/foundation/init/ModEntityTypes.java
@@ -3,33 +3,33 @@ package com.nukateam.ntgl.common.foundation.init;
 import com.nukateam.ntgl.Ntgl;
 import com.nukateam.ntgl.common.foundation.entity.*;
 import com.nukateam.ntgl.common.foundation.entity.misc.AshPile;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredRegister;
 
 /**
  * Author: MrCrayfish
  */
 public class ModEntityTypes {
     public static final DeferredRegister<EntityType<?>> ENTITY_TYPES
-            = DeferredRegister.create(ForgeRegistries.ENTITY_TYPES, Ntgl.MOD_ID);
+            = DeferredRegister.create(Registries.ENTITY_TYPE, Ntgl.MOD_ID);
 
-    public static final RegistryObject<EntityType<AshPile>> ASH_PILE =
+    public static final DeferredHolder<EntityType<?>, EntityType<AshPile>> ASH_PILE =
             registerEntity("ash_pile", EntityType.Builder
                     .<AshPile>of(AshPile::new, MobCategory.MISC)
                     .sized(0.8f, 0.2f));
 
-    public static final RegistryObject<EntityType<FlyingGib>> FLYING_GIBS =
+    public static final DeferredHolder<EntityType<?>, EntityType<FlyingGib>> FLYING_GIBS =
             registerEntity("flying_gibs", EntityType.Builder
                     .<FlyingGib>of(FlyingGib::new, MobCategory.MISC)
                     .sized(0.5f, 0.5f));
 
-    private static <T extends Entity> RegistryObject<EntityType<T>> registerEntity(String entityName, EntityType.Builder<T> builder) {
+    private static <T extends Entity> DeferredHolder<EntityType<?>, EntityType<T>> registerEntity(String entityName, EntityType.Builder<T> builder) {
         return ENTITY_TYPES.register(entityName, () -> builder.build(ResourceLocation.tryBuild(Ntgl.MOD_ID, entityName).toString()));
     }
 
