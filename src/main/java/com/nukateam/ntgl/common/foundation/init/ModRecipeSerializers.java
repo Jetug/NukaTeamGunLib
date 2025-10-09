244966f2a764cacfb34e86868b7583ebe0a38aaf
Diego32099 <diegomaciel3000@gmail.com>
2025-10-08 21:46:09 -0300

    Update ModRecipeSerializers.java to 1.21.1 neoforge

--- src/main/java/com/nukateam/ntgl/common/foundation/init/ModRecipeSerializers.java
+++ src/main/java/com/nukateam/ntgl/common/foundation/init/ModRecipeSerializers.java
@@ -3,18 +3,18 @@ package com.nukateam.ntgl.common.foundation.init;
 import com.nukateam.ntgl.Ntgl;
 import com.nukateam.ntgl.common.foundation.crafting.DyeItemRecipe;
 import com.nukateam.ntgl.common.foundation.crafting.WorkbenchRecipeSerializer;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.item.crafting.SimpleCraftingRecipeSerializer;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredRegister;
 
 /**
  * Author: MrCrayfish
  */
 public class ModRecipeSerializers {
-    public static final DeferredRegister<RecipeSerializer<?>> REGISTER = DeferredRegister.create(ForgeRegistries.RECIPE_SERIALIZERS, Ntgl.MOD_ID);
+    public static final DeferredRegister<RecipeSerializer<?>> REGISTER = DeferredRegister.create(Registries.RECIPE_SERIALIZER, Ntgl.MOD_ID);
 
-    public static final RegistryObject<SimpleCraftingRecipeSerializer<DyeItemRecipe>> DYE_ITEM = REGISTER.register("dye_item", () -> new SimpleCraftingRecipeSerializer<>(DyeItemRecipe::new));
-    public static final RegistryObject<WorkbenchRecipeSerializer> WORKBENCH = REGISTER.register("workbench", WorkbenchRecipeSerializer::new);
+    public static final DeferredHolder<RecipeSerializer<?>, SimpleCraftingRecipeSerializer<DyeItemRecipe>> DYE_ITEM = REGISTER.register("dye_item", () -> new SimpleCraftingRecipeSerializer<>(DyeItemRecipe::new));
+    public static final DeferredHolder<RecipeSerializer<?>, WorkbenchRecipeSerializer> WORKBENCH = REGISTER.register("workbench", WorkbenchRecipeSerializer::new);
 }
