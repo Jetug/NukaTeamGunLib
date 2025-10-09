e7439374c6ee62908aa00161eb7f4c617302a52e
Diego32099 <diegomaciel3000@gmail.com>
2025-10-09 16:38:13 -0300

    Update DynamicGeoItem.java Changed some imports so i can see the new errors

--- src/main/java/com/nukateam/geo/interfaces/DynamicGeoItem.java
+++ src/main/java/com/nukateam/geo/interfaces/DynamicGeoItem.java
@@ -3,12 +3,12 @@ package com.nukateam.geo.interfaces;
 import com.nukateam.geo.render.DynamicGeoItemRenderer;
 import com.nukateam.geo.render.ItemAnimator;
 import com.nukateam.geo.render.ProxyItemRenderer;
-import mod.azure.azurelib.animatable.GeoItem;
-import mod.azure.azurelib.animatable.client.RenderProvider;
+import mod.azure.azurelib.common.api.common.animatable.GeoItem;
+import mod.azure.azurelib.common.internal.client.RenderProvider;
 import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
 import net.minecraft.world.item.ItemDisplayContext;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.api.distmarker.OnlyIn;
 
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
