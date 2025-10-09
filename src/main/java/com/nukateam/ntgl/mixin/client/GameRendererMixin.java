febbb50fe06763586372b484ded831dd426510c1
Diego32099 <diegomaciel3000@gmail.com>
2025-10-08 21:36:18 -0300

    Update GameRendererMixin.java to 1.21.1 neoforge

--- src/main/java/com/nukateam/ntgl/mixin/client/GameRendererMixin.java
+++ src/main/java/com/nukateam/ntgl/mixin/client/GameRendererMixin.java
@@ -1,13 +1,11 @@
 package com.nukateam.ntgl.mixin.client;
 
-import com.mojang.blaze3d.platform.Window;
 import com.nukateam.ntgl.Config;
 import com.nukateam.ntgl.common.foundation.init.ModEffects;
+import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.renderer.GameRenderer;
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -16,13 +14,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(GameRenderer.class)
 public class GameRendererMixin {
     @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/profiling/ProfilerFiller;popPush(Ljava/lang/String;)V", ordinal = 0, shift = At.Shift.AFTER))
-    public void updateCameraAndRender(float partialTicks, long nanoTime, boolean renderWorldIn, CallbackInfo ci) {
+    public void updateCameraAndRender(DeltaTracker deltaTracker, boolean renderLevel, CallbackInfo ci) {
         var minecraft = Minecraft.getInstance();
         var player = minecraft.player;
 
         if (player == null) return;
 
-        var effect = player.getEffect(ModEffects.BLINDED.get());
+        var effect = player.getEffect(ModEffects.BLINDED);
         if (effect != null) {
             // Render white screen-filling overlay at full alpha effect when duration is above threshold
             // When below threshold, fade to full transparency as duration approaches 0
