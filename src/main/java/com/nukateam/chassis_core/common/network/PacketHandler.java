61ba68f35349ed1e48d19e866f929e71ca487079
Jetug <ksergeev01@gmail.com>
2025-11-14 02:08:00 +0300

    cleanup

--- src/main/java/com/nukateam/chassis_core/common/network/PacketHandler.java
+++ src/main/java/com/nukateam/chassis_core/common/network/PacketHandler.java
@@ -1,23 +1,11 @@
 package com.nukateam.chassis_core.common.network;
 
-import com.mrcrayfish.framework.api.network.MessageContext;
-import com.mrcrayfish.framework.api.network.message.PlayMessage;
-import com.mrcrayfish.framework.network.message.IMessage;
-import com.mrcrayfish.framework.platform.network.ForgeMessageContext;
-import com.mrcrayfish.framework.platform.network.ForgeNetworkBuilder;
 import com.nukateam.chassis_core.ChassisCore;
-import com.nukateam.chassis_core.common.network.managers.*;
 import com.nukateam.chassis_core.common.network.packet.*;
 import com.mrcrayfish.framework.api.FrameworkAPI;
 import com.mrcrayfish.framework.api.network.FrameworkNetwork;
 import com.mrcrayfish.framework.api.network.MessageDirection;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraftforge.network.NetworkDirection;
-
-import javax.annotation.Nullable;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.util.Optional;
 
 public class PacketHandler {
     private static FrameworkNetwork PLAY_CHANNEL;
@@ -36,6 +24,4 @@ public class PacketHandler {
                 .registerPlayMessage(S2CMessageUpdateEquipmentConfig.class, MessageDirection.PLAY_CLIENT_BOUND)
                 .build();
     }
-
-
 }
