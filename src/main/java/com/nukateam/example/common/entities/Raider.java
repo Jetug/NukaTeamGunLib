08589bbb36d004508e18b505dbfc54abfec77a9b
Jetug <ksergeev01@gmail.com>
2026-01-23 17:00:45 +0300

    Improved GunAttackGoal, it now automatically applies to mobs that hold guns

--- src/main/java/com/nukateam/example/common/entities/Raider.java
+++ src/main/java/com/nukateam/example/common/entities/Raider.java
@@ -3,7 +3,6 @@ package com.nukateam.example.common.entities;
 import com.nukateam.example.common.registery.ExampleWeapons;
 import com.nukateam.ntgl.Ntgl;
 import com.nukateam.ntgl.common.foundation.item.WeaponItem;
-import com.nukateam.ntgl.common.util.interfaces.IGunUser;
 import com.nukateam.ntgl.common.foundation.goals.GunAttackGoal;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.syncher.EntityDataAccessor;
@@ -26,7 +25,7 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.ServerLevelAccessor;
 import org.jetbrains.annotations.Nullable;
 
-public class Raider extends PathfinderMob implements IGunUser {
+public class Raider extends PathfinderMob{
     private static final EntityDataAccessor<Integer> DATA_ID_TYPE_VARIANT =
             SynchedEntityData.defineId(Raider.class, EntityDataSerializers.INT);
 
@@ -55,7 +54,7 @@ public class Raider extends PathfinderMob implements IGunUser {
     protected void registerGoals() {
         this.goalSelector.addGoal(8, new LookAtPlayerGoal(this, Player.class, 8.0F));
         this.goalSelector.addGoal(8, new RandomLookAroundGoal(this));
-        this.goalSelector.addGoal(3, new GunAttackGoal<>(this, 1.0D, 20.0F, 40));
+        this.goalSelector.addGoal(3, new GunAttackGoal(this, 1.0D, 1.0F, 40));
         this.goalSelector.addGoal(3, new MeleeAttackGoal(this, 1.0D, false));
         this.goalSelector.addGoal(1, new FloatGoal(this));
         this.goalSelector.addGoal(6, new WaterAvoidingRandomStrollGoal(this, 1.0D));
@@ -100,17 +99,7 @@ public class Raider extends PathfinderMob implements IGunUser {
         this.entityData.set(DATA_ID_TYPE_VARIANT, pTypeVariant);
     }
 
-    @Override
-    public void performRangedAttack(LivingEntity pTarget, float pVelocity) {
-        GunAttackGoal.shoot(this, InteractionHand.MAIN_HAND);
-    }
-
     public ResourceLocation getTexture() {
         return ResourceLocation.tryBuild(Ntgl.MOD_ID, "textures/entity/raider.png");
-    }
-
-    @Override
-    public ItemStack getGun() {
-        return getMainHandItem();
     }
 }
