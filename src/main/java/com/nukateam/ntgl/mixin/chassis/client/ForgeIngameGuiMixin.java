c0b009a10257c97311b08ce97cee3bcdb8098c4b
Almecio <almeccio1411@gmail.com>
2026-03-22 07:29:10 +0800

    Hid the vanilla Hunger bar when in power armor because it would be redundant with the AP bar in newly added player hud

--- src/main/java/com/nukateam/ntgl/mixin/chassis/client/ForgeIngameGuiMixin.java
+++ src/main/java/com/nukateam/ntgl/mixin/chassis/client/ForgeIngameGuiMixin.java
@@ -8,6 +8,7 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.gui.overlay.ForgeGui;
 import net.minecraftforge.client.gui.overlay.IGuiOverlay;
+import net.minecraftforge.fml.ModList;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -16,6 +17,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ForgeGui.class)
 @OnlyIn(Dist.CLIENT)
 public class ForgeIngameGuiMixin extends Gui {
+    private static final boolean NUKACRAFT_LOADED = ModList.get() != null && ModList.get().isLoaded("nukacraft");
+
     public ForgeIngameGuiMixin(Minecraft mc) {
         super(mc, mc.getItemRenderer());
     }
@@ -27,6 +30,8 @@ public class ForgeIngameGuiMixin extends Gui {
 
     @Inject(method = "render(Lnet/minecraft/client/gui/GuiGraphics;F)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/RandomSource;setSeed(J)V"))
     public void render(GuiGraphics graphics, float partialTick, CallbackInfo ci) {
+        if (NUKACRAFT_LOADED) return;
+
         IGuiOverlay overlay = (gui, poseStack1, partialTick1, screenWidth, screenHeight) -> {
             var minecraft = Minecraft.getInstance();
             if (PlayerUtils.isLocalWearingChassis() && !minecraft.options.hideGui && gui.shouldDrawSurvivalElements()) {
@@ -37,14 +42,4 @@ public class ForgeIngameGuiMixin extends Gui {
 
         overlay.render((ForgeGui) (Gui) this, graphics, partialTick, screenWidth, screenHeight);
     }
-//
-//    @Final
-//    @Shadow(remap = false)
-//    public static final IIngameOverlay FOOD_LEVEL_ELEMENT = OverlayRegistry.registerOverlayTop("Food Level", (gui, poseStack, partialTick, screenWidth, screenHeight) -> {
-//        var minecraft = Minecraft.getInstance();
-//        if (PlayerUtils.isLocalWearingChassis() && !minecraft.options.hideGui && gui.shouldDrawSurvivalElements()) {
-//            gui.setupOverlayRenderState(true, false);
-//            gui.renderFood(screenWidth, screenHeight, poseStack);
-//        }
-//    });
 }
